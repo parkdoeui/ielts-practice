a1bdd7f9f2d93ac5aa2b6a4df204b578f762e16c
Do Park <do.eui.park@gmail.com>
2026-05-03 16:35:13 -0700

    update crawler/parser.py

--- crawler/parser.py
+++ crawler/parser.py
@@ -504,7 +504,9 @@ def _build_question_groups(groups: list[dict], answers: dict) -> list[QuestionGr
 
         elif (("contain" in instr_lower and ("paragraph" in instr_lower or "section" in instr_lower))
               or ("which" in instr_lower and ("paragraph" in instr_lower or "section" in instr_lower))
-              or ("match" in instr_lower and ("person" in instr_lower or "people" in instr_lower or "statement" in instr_lower))):
+              or ("match" in instr_lower and ("person" in instr_lower or "people" in instr_lower
+                                              or "statement" in instr_lower or "nationality" in instr_lower
+                                              or "researcher" in instr_lower or "event" in instr_lower))):
             if options:
                 para_options = options
             else:
