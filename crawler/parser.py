2f2a2a27be29c16f1691615c377f9089ccf364c7
Do Park <do.eui.park@gmail.com>
2026-05-03 19:51:51 -0700

    update crawler/parser.py

--- crawler/parser.py
+++ crawler/parser.py
@@ -398,9 +398,9 @@ def _parse_children_text(header: str, children_text: str, start_q: int, end_q: i
     stem_lines = []
 
     numbered_pattern = re.compile(r"^(\d+)\.?\s+(.*)")
-    option_inline_pattern = re.compile(r"^([A-K])\s+(.*)")
+    option_inline_pattern = re.compile(r"^([A-Z])\s+(.*)")
     heading_option_pattern = re.compile(r"^(i{1,3}|iv|vi{0,3}|ix|x)\.?\s+(.*)")
-    option_standalone_pattern = re.compile(r"^([A-K])$")
+    option_standalone_pattern = re.compile(r"^([A-Z])$")
     instruction_kw_pattern = re.compile(r"^(YES|NO|TRUE|FALSE|NOT GIVEN)\b", re.IGNORECASE)
     instruction_cont_pattern = re.compile(r"^if (the statement|there is|it is)", re.IGNORECASE)
 
