a2e39168d387a7feffb434763129066a23926678
Do Park <do.eui.park@gmail.com>
2026-08-01 21:38:23 -0700

    update frontend/src/components/ReadingTest.tsx

--- frontend/src/components/ReadingTest.tsx
+++ frontend/src/components/ReadingTest.tsx
@@ -394,7 +394,10 @@ export function ReadingTest({ embeddedTestId, onComplete }: ReadingTestProps = {
       </div>
 
       {/* Main layout */}
-      <div className="flex flex-1 min-h-0">
+      <div
+        className="flex flex-1 min-h-0"
+        style={embedded ? ({ zoom: FONT_SCALE_ZOOM[fontScale] } as React.CSSProperties) : undefined}
+      >
         {/* Passage panel */}
         <div
           className={`
